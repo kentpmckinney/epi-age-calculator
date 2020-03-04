584053071539ac27c86f541add0b5f042b5ccd6b
Kent McKinney <kent.p.mckinney@gmail.com>
2020-03-03 19:30:46 -0800

    add test for marsYears

--- src/age.js
+++ src/age.js
@@ -4,7 +4,7 @@ export class Age {
     this.earthYears = earthYears;
     this.mercuryYears = this.calculateMercuryYears(earthYears);
     this.venusYears = this.calculateVenusYears(earthYears);
-    this.marsYears = this.calculateMarsYears(earthYears);
+    
     this.jupiterYears = this.calculateJupiterYears(earthYears);
   }
 
@@ -16,9 +16,7 @@ export class Age {
     return parseFloat((earthYears/.62).toFixed(2));
   }
   
-  calculateMarsYears(earthYears) {
-    return parseFloat((earthYears/1.88).toFixed(2));
-  }
+
 
   calculateJupiterYears(earthYears) {
     return parseFloat((earthYears/11.86 ).toFixed(2));
