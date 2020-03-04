00c0a3936ae4b0ed8575e66032edb78f23619159
Kent McKinney <kent.p.mckinney@gmail.com>
2020-03-03 19:03:58 -0800

    add test for mercuryYears

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,18 +1,18 @@
 import { Age } from '../src/age.js';
 
 describe('The Age class', () => {
-	let age;
 
 	beforeEach(() => {
-		age = new Age(10);
 	});
 
 	test('Class constructor accept a persons age in Earth years and creates an valid object', () => {
+		let age = new Age(10);
 		expect(age.earthYears).toEqual(10);
 	});
 
 	test('Class constructor calls a function to return age in Mercury years (age/.24 Earth years)', () => {
-		
+		let age = new Age(42);
+		expect(age.mercuryYears).toEqual(175);
 	});
 
 	test('Class constructor calls a function to return age in Venus years (age/.62 Earth years)', () => {
