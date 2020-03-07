d8b6c9ed0f1ee299c40194b444991098cc2b389b
Kent McKinney <kent.p.mckinney@gmail.com>
2020-03-07 09:35:56 -0800

    add user interface (WIP)

--- src/main.js
+++ src/main.js
@@ -1,16 +1,35 @@
+/* This file is the webpack entry point */
 import { Age } from './age.js';
 import $ from 'jquery';
 
-let age = new Age();
-console.log(age.earthYears)
+/* Business Logic */
+let age;
 
+/* User Interface */
 $(document).ready(function(){
   
   $('form').submit(function(e){
     let earthYears = $('#age-earth-years').val();
     let avgLifeExpectancy = $('#avg-life-expectancy').val();
-    
+    age = new Age(earthYears);
+    age.avgLifeExpectancy = avgLifeExpectancy;
+    $('#results').empty();
+    $('#results').append(`Results for age ${age.earthYears} in Earth years and average life expectancy of ${age.avgLifeExpectancy}:<br>`);
+    $('#results').append(`Age in Earth Years: ${age.earthYears}`);
     e.preventDefault();
   })
 
 });
+
+// this.earthYears = earthYears;
+//     this.earthYearsLeft;
+//     this.mercuryYears = this.calculateMercuryYears(earthYears);
+//     this.mercuryYearsLeft;
+//     this.venusYears = this.calculateVenusYears(earthYears);
+//     this.venusYearsLeft;
+//     this.marsYears = this.calculateMarsYears(earthYears);
+//     this.marsYearsLeft;
+//     this.jupiterYears = this.calculateJupiterYears(earthYears);
+//     this.jupiterYearsLeft;
+//     this._avgLifeExpectancy;
+//     this.earthYearsBeyondLifeExpectancy;
